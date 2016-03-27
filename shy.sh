fc8aa4695cef726d89ddfb2e5d6aadd0184feac9
Simmo Saan <simmo.saan@gmail.com>
2016-03-27 23:03:21 +0300

    Bump version number for Phase 2

--- shy.sh
+++ shy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # find script directory over symlinks - http://stackoverflow.com/a/1482133/854540
 DIR="$(dirname "$(readlink -f "$0")")"
-java -jar "$DIR/target/shy-1.0.jar" "$@"
+java -jar "$DIR/target/shy-2.0-SNAPSHOT.jar" "$@"
