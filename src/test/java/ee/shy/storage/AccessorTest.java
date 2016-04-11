2b37604ead09b76992877e3c16a409ecc36f3722
Simmo Saan <simmo.saan@gmail.com>
2016-04-11 10:07:12 +0300

    Parametrize accessor tests

--- src/test/java/ee/shy/storage/AccessorTest.java
+++ src/test/java/ee/shy/storage/AccessorTest.java
@@ -6,64 +6,71 @@ import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class AccessorTest {
     private static final String TEST_STRING = "Hello, World!";
 
     @Rule
     public final TemporaryDirectory temporaryDirectory = new TemporaryDirectory();
 
+    private FileAccessor accessor;
+    private Predicate<Path> fileChecker;
+
     private Path file;
+
+    @Parameters
+    public static Collection<Object[]> accessors() {
+        Supplier<FileAccessor> plainSupplier = () -> new PlainFileAccessor();
+        Predicate<Path> plainChecker = path -> Files.isRegularFile(path);
+
+        Supplier<FileAccessor> gzipSupplier = () -> new GzipFileAccessor();
+        Predicate<Path> gzipChecker = path -> Files.isRegularFile(PathUtils.addExtension(path, ".gz"));
+
+        Supplier<FileAccessor> aggregateSupplier = () -> new AggregateFileAccessor(Arrays.asList(
+                gzipSupplier.get(),
+                plainSupplier.get()
+        ));
+
+        return Arrays.asList(new Object[][]{
+                {plainSupplier, plainChecker},
+                {gzipSupplier, gzipChecker},
+                {aggregateSupplier, gzipChecker}
+        });
+    }
+
+    public AccessorTest(Supplier<FileAccessor> accessorSupplier, Predicate<Path> fileChecker) {
+        this.accessor = accessorSupplier.get();
+        this.fileChecker = fileChecker;
+    }
 
     @Before
     public void setUp() throws Exception {
         file = temporaryDirectory.newFile();
+        Files.delete(file);
     }
 
     @Test
-    public void testPlainAccessor() throws IOException {
-        FileAccessor accessor = new PlainFileAccessor();
-
+    public void testRetrieve() throws Exception {
         accessor.add(file, IOUtils.toInputStream(TEST_STRING));
-        assertTrue(Files.isRegularFile(file));
+        assertTrue(fileChecker.test(file));
         assertEquals(TEST_STRING, IOUtils.toString(accessor.get(file)));
-
-        assertNull(accessor.get(Paths.get("non-existent")));
     }
 
     @Test
-    public void testGzipAccessor() throws IOException {
-        FileAccessor accessor = new GzipFileAccessor();
-
-        accessor.add(file, IOUtils.toInputStream(TEST_STRING));
-        Path extendedFile = PathUtils.addExtension(file, ".gz");
-        assertTrue(Files.isRegularFile(extendedFile));
-        assertEquals(TEST_STRING, IOUtils.toString(accessor.get(file)));
-
-        assertNull(accessor.get(Paths.get("non-existent")));
-    }
-
-    @Test
-    public void testAggregateAccessor() throws IOException {
-        FileAccessor writeAccessor = new PlainFileAccessor();
-
-        writeAccessor.add(file, IOUtils.toInputStream(TEST_STRING));
-
-        FileAccessor readAccessor = new AggregateFileAccessor(Arrays.asList(
-                new GzipFileAccessor(),
-                new PlainFileAccessor()
-        ));
-
-        assertEquals(TEST_STRING, IOUtils.toString(readAccessor.get(file)));
-
-        assertNull(readAccessor.get(Paths.get("non-existent")));
+    public void testNonExistent() throws Exception {
+        assertNull(accessor.get(file));
     }
 }
