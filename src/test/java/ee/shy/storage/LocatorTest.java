39c512412c6fe09afd8c276b03d44977f5e8dc1e
Simmo Saan <simmo.saan@gmail.com>
2016-04-11 11:26:09 +0300

    Increase FileLocator test coverage

--- src/test/java/ee/shy/storage/LocatorTest.java
+++ src/test/java/ee/shy/storage/LocatorTest.java
@@ -6,9 +6,10 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class LocatorTest {
     @Rule
@@ -25,33 +26,52 @@ public class LocatorTest {
     public void testFlatLocator() throws IOException {
         FileLocator locator = new FlatFileLocator(storageDirectory);
 
-        assertEquals(
-                storageDirectory
-                        .resolve("0000000000000000000000000000000000000000"),
-                locator.locate(Hash.ZERO));
+        Path path = storageDirectory
+                .resolve("0000000000000000000000000000000000000000");
+
+        assertEquals(path, locator.locateAdd(Hash.ZERO));
+        assertEquals(path, locator.locateGet(Hash.ZERO));
     }
 
     @Test
     public void testNestedLocator() throws IOException {
         FileLocator locator = new NestedFileLocator(storageDirectory, 10);
 
-        assertEquals(
-                storageDirectory
-                        .resolve("0000000000")
-                        .resolve("0000000000")
-                        .resolve("0000000000")
-                        .resolve("0000000000"),
-                locator.locate(Hash.ZERO));
+        Path path = storageDirectory
+                .resolve("0000000000")
+                .resolve("0000000000")
+                .resolve("0000000000")
+                .resolve("0000000000");
+
+        assertEquals(path, locator.locateAdd(Hash.ZERO));
+        assertTrue(Files.isDirectory(path.getParent()));
+        assertEquals(path, locator.locateGet(Hash.ZERO));
     }
 
     @Test
     public void testGitLocator() throws IOException {
         FileLocator locator = new GitFileLocator(storageDirectory);
 
-        assertEquals(
-                storageDirectory
-                        .resolve("00")
-                        .resolve("00000000000000000000000000000000000000"),
-                locator.locate(Hash.ZERO));
+        Path path = storageDirectory
+                .resolve("00")
+                .resolve("00000000000000000000000000000000000000");
+
+        assertEquals(path, locator.locateAdd(Hash.ZERO));
+        assertTrue(Files.isDirectory(path.getParent()));
+        assertEquals(path, locator.locateGet(Hash.ZERO));
+    }
+
+    @Test
+    public void testAbstractLocator() throws IOException {
+        FileLocator locator = new AbstractFileLocator(storageDirectory);
+
+        assertNull(locator.locateAdd(Hash.ZERO));
+        assertNull(locator.locateGet(Hash.ZERO));
+    }
+
+    private class AbstractFileLocator extends FileLocator {
+        public AbstractFileLocator(Path root) throws IOException {
+            super(root);
+        }
     }
 }
