d18e6a3b5db1893c316c1d1872c287c228aec2c4
Simmo Saan <simmo.saan@gmail.com>
2016-04-16 17:51:13 +0300

    Test DiffUtils class

--- src/main/java/ee/shy/core/diff/DiffUtils.java
+++ src/main/java/ee/shy/core/diff/DiffUtils.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class DiffUtils {
+public final class DiffUtils {
     /**
      * Map containing character and its corresponding color.
      */
