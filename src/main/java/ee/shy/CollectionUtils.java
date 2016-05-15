a88eb5e649e7a855e64781db05af6ca0932b6a8a
Simmo Saan <simmo.saan@gmail.com>
2016-05-15 19:15:52 +0300

    Extract static array methods from TreePath

--- src/main/java/ee/shy/CollectionUtils.java
+++ src/main/java/ee/shy/CollectionUtils.java
@@ -20,4 +20,42 @@ public final class CollectionUtils {
         newList.addAll(Arrays.asList(items));
         return newList;
     }
+
+    /**
+     * Tests if the array starts with another prefix array.
+     * @param arr array to check prefix in
+     * @param prefix prefix array
+     * @param <T> type of array elements
+     * @return {@code true} if array starts with prefix, {@code false} otherwise
+     */
+    public static <T> boolean startsWith(T[] arr, T[] prefix) {
+        if (arr.length < prefix.length)
+            return false;
+
+        for (int i = 0; i < prefix.length; i++) {
+            if (!arr[i].equals(prefix[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Compares two arrays of comparable elements lexicographically.
+     * @param arr1 first array
+     * @param arr2 second array
+     * @param <T> comparable type of array elements
+     * @return negative integer if {@code arr1} comes before {@code arr2},
+     *         positive integer if {@code arr1} comes after {@code arr2},
+     *         {@code 0} if arrays equal
+     */
+    public static <T extends Comparable<T>> int compare(T[] arr1, T[] arr2) {
+        int length = Math.min(arr1.length, arr2.length);
+        for (int i = 0; i < length; i++) {
+            int compare = arr1[i].compareTo(arr2[i]);
+            if (compare != 0)
+                return compare;
+        }
+        return arr1.length - arr2.length;
+    }
 }
--- src/main/java/ee/shy/core/TreePath.java
+++ src/main/java/ee/shy/core/TreePath.java
@@ -1,8 +1,7 @@
 package ee.shy.core;
 
+import ee.shy.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
-
-import java.util.Arrays;
 
 /**
  * A class that handles trees' paths.
@@ -44,34 +43,22 @@ public class TreePath implements Comparable<TreePath> {
 
     /**
      * Returns whether given path is prefix of this one.
-     * @param path path to check
+     * @param prefix path to check
      * @return true if this starts with given path, false otherwise
      */
-    public boolean startsWith(TreePath path) {
-        return pathStrings.length >= path.pathStrings.length
-                && Arrays.equals(ArrayUtils.subarray(pathStrings, 0, path.pathStrings.length), path.pathStrings);
+    public boolean startsWith(TreePath prefix) {
+        return CollectionUtils.startsWith(pathStrings, prefix.pathStrings);
     }
 
     /**
      * Compares two TreePath objects.
-     * @param o TreePath object to compare to.
+     * @param other TreePath object to compare to.
      * @return a value of 0 if the objects are equal, a value greater than 0 if _ is bigger than _, a value less than
      * 0 if _ is smaller than _ and difference of the objects' string lists length if the objects' strings' values are
      * equal up to object's with shortest string list.
      */
     @Override
-    public int compareTo(TreePath o) {
-        String[] oStringList = o.pathStrings;
-
-        int iterCount = Math.min(pathStrings.length, oStringList.length);
-
-        for (int i = 0; i < iterCount; i++) {
-            int strCompareResult = pathStrings[i].compareTo(oStringList[i]);
-            if(strCompareResult != 0) {
-                return strCompareResult;
-            }
-        }
-
-        return pathStrings.length - oStringList.length;
+    public int compareTo(TreePath other) {
+        return CollectionUtils.compare(pathStrings, other.pathStrings);
     }
 }
