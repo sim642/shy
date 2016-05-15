4ab6edf61f2cf6c9269a17d2ef4315b09f3215d1
Simmo Saan <simmo.saan@gmail.com>
2016-05-16 02:10:36 +0300

    Bump version number for Presentation

--- shy.sh
+++ shy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # find script directory over symlinks - http://stackoverflow.com/a/1482133/854540
 DIR="$(dirname "$(readlink -f "$0")")"
-java -jar "$DIR/app/target/app-3.0.jar" "$@"
+java -jar "$DIR/app/target/app-3.1-SNAPSHOT.jar" "$@"
