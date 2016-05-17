1044e97c58c08528fbfb4a086cdb3aef7c1791e8
Tiit Oja <tiit.oja@gmail.com>
2016-05-18 01:02:23 +0300

    Fix minor code issues

--- app/src/main/java/ee/shy/core/IgnoreChecker.java
+++ app/src/main/java/ee/shy/core/IgnoreChecker.java
@@ -2,13 +2,13 @@ package ee.shy.core;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public final class IgnoreChecker {
 
-    private final List<PathMatcher> pathMatchers = new ArrayList<>();
+    private final List<PathMatcher> pathMatchers;
 
     private final Path rootPath;
 
@@ -16,16 +16,20 @@ public final class IgnoreChecker {
         this.rootPath = rootPath;
         Path shyIgnorePath = rootPath.resolve(".shyignore");
         if (Files.exists(shyIgnorePath)) {
-            pathMatchers.addAll(Files.readAllLines(shyIgnorePath).stream().map(
-                    string -> FileSystems.getDefault().getPathMatcher("glob:" + string)).collect(Collectors.toList())
+            pathMatchers = Files.lines(shyIgnorePath).map(
+                    string -> FileSystems.getDefault().getPathMatcher("glob:" + string)).collect(Collectors.toList()
             );
+        } else {
+            pathMatchers = Collections.EMPTY_LIST;
         }
+
     }
 
     public boolean isIgnored(Path path) throws IOException {
         if (Files.isHidden(path)) return true;
+        Path matchablePath = rootPath.relativize(path.normalize().toAbsolutePath());
         for (PathMatcher pathMatcher : pathMatchers) {
-            if (pathMatcher.matches(rootPath.relativize(path.normalize().toAbsolutePath()))) {
+            if (pathMatcher.matches(matchablePath)) {
                 return true;
             }
         }
